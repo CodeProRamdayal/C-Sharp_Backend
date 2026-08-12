eff800ea9367dee6602bc36874d96671161d2a15
RamDayal Patel <ramdayal.patel45120@gmail.com>
2026-08-12 05:59:19 +0530

    Maked the GET Rest api for fetching the event data.

---
+++ Reactivities/API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
